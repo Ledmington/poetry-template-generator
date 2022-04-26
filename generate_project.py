2ef00f86108805393ac39ee824c8799030e90091
Filippo Barbari <ledmington.dev@gmail.com>
2022-04-26 09:42:23 +0200

    Updated generate_project.py

--- generate_project.py
+++ generate_project.py
@@ -30,25 +30,25 @@ def generate_cov_config(proj_name):
 def generate_makefile(proj_name):
     print("Generating Makefile...")
     makefile = f"""RUN=poetry run
-\n
+
 .PHONY: cov
-\n
+
 update:
 	poetry update
 	poetry lock
-\n
+
 install:
 	poetry install
-\n
+
 cov:
 	${{RUN}} pytest --cov-config=.cov_config --cov-report html:cov_report --cov=./{proj_name} ./{proj_name}/tests
-\n
+
 format:
 	${{RUN}} black .
-\n
+
 todo:
 	find . | grep .py$$ | grep -rnw . -e TODO
-\n
+
 clean:
 	rm -rf cov_report dist .idea .ipynb_checkpoints
 	rm -f .coverage*
@@ -65,15 +65,15 @@ name = "{proj_name}"
 version = "0.1.0"
 description = "Python-Poetry template"
 authors = ["Ledmington <ledmington.dev@gmail.com>"]
-\n
+
 [tool.poetry.dependencies]
 python = "^3.9"
-\n
+
 [tool.poetry.dev-dependencies]
 pytest = "^7.1.1"
 pytest-cov = "^3.0.0"
 black = "^22.3.0"
-\n
+
 [build-system]
 requires = ["poetry-core>=1.0.0"]
 build-backend = "poetry.core.masonry.api"
@@ -83,16 +83,18 @@ build-backend = "poetry.core.masonry.api"
 
 def generate_readme(proj_name):
     print("Generating README.md...")
-    readme = f"# {proj_name}\n\n"
-    """## Local development
-    You need `poetry` installed to develop this software. You can install it with:
-    ```
-    pip install poetry
-    ```
-    After that, you can install the project dependencies with:
-    ```
-    make install
-    ```"""
+    readme = f"""# {proj_name}
+
+## Local development
+You need `poetry` installed to develop this software. You can install it with:
+```
+pip install poetry
+```
+After that, you can install the project dependencies with:
+```
+make install
+```
+    """
     with open("./README.md", "w") as f:
         f.write(readme)
 
@@ -101,7 +103,7 @@ def generate_git_stuff():
     ignore = """cov_report/
 .coverage*
 **/.pytest_cache
-**/__pycache__\n
+**/__pycache__
 dist/
 """
     attributes = "* text=auto eol=lf\n"
@@ -113,33 +115,33 @@ dist/
 def generate_github_workflow(proj_name):
     print("Generating GitHub workflow...")
     workflow = f"""name: {proj_name} tests
-\n
+
 on:
-    workflow_dispatch:
-    push:
-        branches: [ master ]
-\n
+  workflow_dispatch:
+  push:
+    branches: [ master ]
+
 jobs:
-    tests:
-    runs-on: ubuntu-latest
-    strategy:
-        matrix:
-        pv: ['3.8', '3.9', '3.10']
-    steps:
-        - uses: actions/checkout@v2
-        - uses: actions/setup-python@v2
-        with:
-            python-version: ${{{{ matrix.pv }}}}
-            architecture: 'x64'
-\n
-        - name: Install Poetry
-        run: pip install poetry
-\n
-        - name: Install dependencies
-        run: make install
-\n
-        - name: Tests
-        run: make cov
+  tests:
+  runs-on: ubuntu-latest
+  strategy:
+    matrix:
+    pv: ['3.8', '3.9', '3.10']
+  steps:
+    - uses: actions/checkout@v2
+    - uses: actions/setup-python@v2
+    with:
+      python-version: ${{{{ matrix.pv }}}}
+      architecture: 'x64'
+
+    - name: Install Poetry
+      run: pip install poetry
+
+    - name: Install dependencies
+      run: make install
+
+    - name: Tests
+      run: make cov
 """
     os.mkdir(".github")
     os.mkdir(".github/workflows")
